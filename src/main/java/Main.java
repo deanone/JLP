b4e6bee393da9e2131ae76174be836f32bb47868
deanone <athanasios.salamanis@gmail.com>
2024-01-20 20:44:51 +0200

    Checking indentation (2).

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 package main.java;
 
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         int N = 3;
         NewArrayImpl nArr = new NewArrayImpl(N);
         nArr.add(0, 2);
@@ -9,11 +9,11 @@ public class Main {
         nArr.add(2, 1);
         String nArrStr = nArr.toString();
         System.out.println("String representation: " + nArrStr);
-        
+
         int key = 1;
         boolean found = nArr.exists(key);
         if (found) {
-		    System.out.println("Key exists.");
+            System.out.println("Key exists.");
         } else {
             System.out.println("Key does not exist.");
         }
--- src/main/java/MathUtil.java
+++ src/main/java/MathUtil.java
@@ -3,32 +3,32 @@ package main.java;
 import java.util.Arrays;
 
 class MathUtil {
-	
-	public static boolean linearSearch(int[] arr, int key) {
-		boolean found = false;
-		for (int elem : arr) {
-			if (key == elem) {
-				found = true;
-				break;
-			}
-		} 
-		return found;
-	}
-	
-	public static boolean binarySearch(int[] arr, int key, int start, int end) {
-		Arrays.sort(arr);
-		
-		if (key < arr[start] || key > arr[end]) {
-			return false;
-		}
-		
-		int mid = (start + end) / 2;
-		if (key == arr[mid]) {
-			return true;
-		} else if (key < arr[mid]) {
-			return binarySearch(arr, key, start, mid - 1);
-		} else {
-			return binarySearch(arr, key, mid + 1, end);
-		}
-	}
+
+    public static boolean linearSearch(int[] arr, int key) {
+        boolean found = false;
+        for (int elem : arr) {
+            if (key == elem) {
+                found = true;
+                break;
+            }
+        } 
+        return found;
+    }
+
+    public static boolean binarySearch(int[] arr, int key, int start, int end) {
+        Arrays.sort(arr);
+
+        if (key < arr[start] || key > arr[end]) {
+            return false;
+        }
+
+        int mid = (start + end) / 2;
+        if (key == arr[mid]) {
+            return true;
+        } else if (key < arr[mid]) {
+            return binarySearch(arr, key, start, mid - 1);
+        } else {
+            return binarySearch(arr, key, mid + 1, end);
+        }
+    }
 }
